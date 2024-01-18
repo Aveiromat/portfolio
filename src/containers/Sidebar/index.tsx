75ef7b73c89108f29c923bb3c19e3afc20f925e2
Aveiro <matheusaveirofagundes@gmail.com>
2024-01-18 09:48:53 -0300

    prettier error

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/aveiromat.png" />
+    <Titulo fontSize={20}>Matheus Aveiro</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,15 @@
+import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
+
+const Sobre = () => (
+  <section>
+    <Titulo fontSize={16}>Sobre mim</Titulo>
+    <Paragrafo tipo="secundario">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit soluta
+      quidem eum deleniti et qui. Sit ab tempora eligendi vero eaque accusantium
+      corrupti enim delectus voluptatem error odio, soluta distinctio.
+    </Paragrafo>
+  </section>
+)
+
+export default Sobre
